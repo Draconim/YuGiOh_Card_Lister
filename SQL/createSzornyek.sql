a03619be055e36e9f3e586522befa53b86ab48ce
Barankai László Ferenc <barankailaci@gmail.com>
2020-12-13 19:23:14 +0100

    Azonosító és szorny_type módosítva.
    Azonosító char(9) -> varchar2(10)
    szorny_type -> monster_card_type

--- SQL/createSzornyek.sql
+++ SQL/createSzornyek.sql
@@ -1,9 +1,9 @@
 drop table szornyek;
 CREATE TABLE szornyek(
-    azonosito char(9) not null,
+    azonosito varchar2(10) not null,
     nev varchar2(30) not null,
     leiras varchar2(300),
-    szorny_type varchar2(10) not null,
+    monster_card_type varchar2(10) not null,
     monster_level int,
     monster_attribute varchar2(15) not null,
     monster_type varchar2(10) not null,
@@ -12,10 +12,10 @@ CREATE TABLE szornyek(
     link_level int,
     rarity varchar2(15) not null,
     quantity int not null,
- 
+
     
     constraint pk_szornyek primary key(azonosito),
-    constraint ch_szorny_tipus check(szorny_type in ('normal', 'effect', 'synchro', 'xyz', 'link', 'ritual')),
+    constraint ch_monster_card_type check(monster_card_type in ('normal', 'effect', 'synchro', 'xyz', 'link', 'ritual')),
     constraint ch_attribute check(monster_attribute in ('light', 'dark', 'earth', 'water', 'wind', 'fire', 'divine-beast')),
     constraint ch_rarity check(rarity in ('common', 'rare', 'super-rare', 'ultra-rare', 'secrete-rare'))
 );
