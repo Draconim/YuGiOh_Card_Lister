ca1624ab8fc3930afd1cbb65cffbe09337a3a6ec
Barankai László Ferenc <barankailaci@gmail.com>
2020-12-13 19:24:14 +0100

    Azonosító módosítva
    char(9) -> varchar2(10)

--- SQL/createVarazs.sql
+++ SQL/createVarazs.sql
@@ -1,7 +1,7 @@
 drop table varazslap;
 create table varazslap
 (
-    azonosito char(9) not null,
+    azonosito varchar2(10) not null,
     nev varchar2(30) not null,
     leiras varchar2(300),
     magic_type varchar2(11) not null,
@@ -12,4 +12,3 @@ create table varazslap
     constraint ch_magic_type check(magic_type in ('normal', 'quick-play', 'continous', 'field', 'equip', 'ritual')),
     constraint ch_varazs_rarity check(rarity in ('common', 'rare', 'super-rare', 'ultra-rare', 'secrete-rare'))
 );
-    
