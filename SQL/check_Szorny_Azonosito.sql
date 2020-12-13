4c5a399b79f84b8e6b43aed6e7c020cef0f4297e
Barankai László Ferenc <barankailaci@gmail.com>
2020-12-13 19:19:51 +0100

    Update check_Szorny_Azonosito.sql
    Hibák javítva.

--- SQL/check_Szorny_Azonosito.sql
+++ SQL/check_Szorny_Azonosito.sql
@@ -12,11 +12,11 @@ begin
         return 0;
     end if;
 
-    if length(trim(p_rendszam)) = 10 then
+    if length(trim(p_azonosito)) = 10 then
         v_i := 1;
         while v_i <= 2 loop
             v_azonosito_char := substr(p_azonosito, v_i, 1);
-            if not (ascii('A') <= ascii(v_azonosito_char) and ascii(v_azonosito_char) <= ascii('Z')) or then
+            if not (ascii('A') <= ascii(v_azonosito_char) and ascii(v_azonosito_char) <= ascii('Z')) then
                 return 0;            
             end if;
             v_i := v_i + 1;
